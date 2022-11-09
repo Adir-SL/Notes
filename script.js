369b314431488efe29590cbb846e24ffe16addea
Adir <slutzkis@gmail.com>
2022-11-09 11:26:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ function loadFunc() {
   }
   function openDialog() {
     document.getElementById("deleteDialog").style.display = "block";
-    // window.setfo
+    document.getElementById("cancelBtn")
   }
   function closeDialog() {
     document.getElementById("deleteDialog").style.display = "none";
